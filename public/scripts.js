e8db2e459b433710017c1cdc33660d7675704f33
Mr.Nobody <mboryczko0@gmail.com>
2021-09-11 23:33:08 +0200

    files updated

--- public/scripts.js
+++ public/scripts.js
@@ -3,70 +3,73 @@ let i = 1;
 
 const onChange = (e, i) => {
   i = i - 1;
-
+  const someStringToCheck = e.currentTarget.id;
+  const indexOfSpace = someStringToCheck.indexOf("-");
+  const index = parseInt(
+    someStringToCheck.substr(indexOfSpace + 1, someStringToCheck.length + 1)
+  );
+  let boxDiv = document.getElementById(`box-${index}`);
+  let heightValue = document.getElementById(`height-${index}`).value + "px";
+  let widthValue = document.getElementById(`width-${index}`).value + "px";
   if (e.target.checked) {
-    const someStringToCheck = e.currentTarget.id;
-    const indexOfSpace = someStringToCheck.indexOf("-");
-    const index = parseInt(
-      someStringToCheck.substr(indexOfSpace + 1, someStringToCheck.length + 1)
-    );
-    document.getElementById(`box-${i}`);
-    let heightValue = document.getElementById(`height-${index}`).value + "px";
-    let WidthValue = document.getElementById(`width-${index}`).value + "px";
-    console.log("checkbox number -" + index);
-    document.getElementById(`box-${index}`).style.width = WidthValue;
-    document.getElementById(`box-${index}`).style.height = heightValue;
-    document.getElementById(`box-${index}`).style.lineHeight = heightValue;
+    boxDiv.style.width = widthValue;
+    boxDiv.style.height = heightValue;
   } else {
-    const someStringToCheck = e.currentTarget.id;
-    const indexOfSpace = someStringToCheck.indexOf("-");
-    const index = parseInt(
-      someStringToCheck.substr(indexOfSpace + 1, someStringToCheck.length + 1)
-    );
-    document.getElementById(`box-${index}`).style.width = "100px";
-    document.getElementById(`box-${index}`).style.height = "100px";
-    document.getElementById(`box-${index}`).style.lineHeight = "100px";
+    boxDiv.style.width = "100px";
+    boxDiv.style.height = "100px";
   }
 };
 
+function instantSizeChange(e, i) {
+  i = i - 1;
+  const someStringToCheck = e;
+  const indexOfSpace = someStringToCheck.indexOf("-");
+  const index = parseInt(
+    someStringToCheck.substr(indexOfSpace + 1, someStringToCheck.length + 1)
+  );
+  let boxDiv = document.getElementById(`box-${index}`);
+  let heightValue = document.getElementById(`height-${index}`).value + "px";
+  let widthValue = document.getElementById(`width-${index}`).value + "px";
+  boxDiv.style.width = widthValue;
+  boxDiv.style.height = heightValue;
+  document.getElementById(`checkbox-${i}`).checked = true;
+}
+
 function addbox() {
-  let divMainBox = document.createElement("div");
+  const divMainBox = document.createElement("div");
+  const divBixBox = document.createElement("div");
+  const divSmallBox = document.createElement("div");
   divMainBox.id = `mainBox-${i}`;
   divMainBox.className = "mainBox";
-  let divDtBorder = document.createElement("div");
-  divDtBorder.className = "dtborder";
-  divDtBorder.id = `box-${i}`;
-  divDtBorder.innerText = "Box";
-  let divDtBorder_two = document.createElement("div");
-  divDtBorder_two.className = "dtborder_two";
-  let divDtBorder_three = document.createElement("div");
-  divDtBorder_three.className = "dtborder_three";
-  let divDtBorder_four = document.createElement("div");
-  divDtBorder_four.className = "dtborder_four";
+  divBixBox.className = "bigBox";
+  divSmallBox.id = `box-${i}`;
+  divSmallBox.className = "smallbox";
+  divSmallBox.innerText = "Box";
   document.getElementById("main").append(divMainBox);
-  divMainBox.appendChild(divDtBorder);
-  divMainBox.appendChild(divDtBorder_two);
-  divMainBox.appendChild(divDtBorder_three);
-  divMainBox.appendChild(divDtBorder_four);
+  divMainBox.append(divBixBox);
+  divBixBox.appendChild(divSmallBox);
 
-  let widthBox = document.createElement("input");
+  const widthBox = document.createElement("input");
+  const heightBox = document.createElement("input");
+  let mainBoxDib = document.getElementById(`mainBox-${i}`);
   widthBox.id = `width-${i}`;
   widthBox.className = "data";
   widthBox.type = "number";
   widthBox.placeholder = "Width";
-  document.getElementById(`mainBox-${i}`).append(widthBox);
-  let heightBox = document.createElement("input");
+  widthBox.addEventListener("input", () => instantSizeChange(widthBox.id, i));
+  mainBoxDib.append(widthBox);
   heightBox.id = `height-${i}`;
   heightBox.className = "data";
   heightBox.type = "number";
   heightBox.placeholder = "Height";
-  document.getElementById(`mainBox-${i}`).append(heightBox);
+  heightBox.addEventListener("input", () => instantSizeChange(widthBox.id, i));
+  mainBoxDib.append(heightBox);
 
-  let checkBoxDiv = document.createElement("div");
-  let checkBox = document.createElement("input");
+  const checkBoxDiv = document.createElement("div");
+  const checkBox = document.createElement("input");
+  const checkboxLabel = document.createElement("label");
   checkBox.id = `checkbox-${i}`;
   checkBox.type = "checkbox";
-  let checkboxLabel = document.createElement("label");
   checkboxLabel.setAttribute("for", checkBox.id);
   checkboxLabel.innerText = "Box sizing ?";
   checkBox.addEventListener("change", (e) => onChange(e, i));
